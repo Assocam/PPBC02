c8672fa38adf5e972634912adac60fa1b4789818
smat90 <smat90@hotmail.it>
2024-03-26 21:24:45 +0100

    2603

--- _personale/classi.py
+++ _personale/classi.py
@@ -1,3 +1,13 @@
-a=[1,2,3]
-b=[1,1,1]
-print(a*2)
+import csv
+# #with open(r"C:\PPBC02\PPBC02\files_esercizi\botteghe-storiche.csv","r") as file_in:
+# with open("./files_esercizi/botteghe-storiche.csv","r",encoding='utf-8') as file_in:
+#     File_reader=csv.DictReader(file_in,delimiter=",")
+#     for _ in range(10):
+#         linea=next(file_in)
+#         print(linea, end='')
+
+with open("./files_esercizi/botteghe-storiche.csv","r",encoding='utf-8') as file_in:
+    File_reader=csv.DictReader(file_in,delimiter=",")
+    for _ in File_reader:
+        if _['ID'] in ["45","46"]:
+            print(_["ID"],_['Ragione sociale'])
