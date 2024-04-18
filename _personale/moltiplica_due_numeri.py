44b09a9be0d78f5bc8f35b306b3780e8b32dd93f
Helena <helenagkraus@gmail.com>
2024-04-18 21:23:42 +0200

    s

--- _personale/moltiplica_due_numeri.py
+++ _personale/moltiplica_due_numeri.py
@@ -8,9 +8,15 @@ else:
     try:
         primo_numero = float(sys.argv[1])
         
-    except ValueError
+    except ValueError:
+        print("Hai inserito un valore che non può essere convertito in float")
+
+    try: 
+        secondo_numero = float(sys.argv[2])
+    except ValueError:
+        print("Hai inserito un valore che non può essere convertito in float")
 
 
-secondo_numero = float(sys.argv[2])
+
     risultato = primo_numero * secondo_numero
     print(f"il risultato di {primo_numero} per {secondo_numero} è uguale a {risultato}")
