9ff3d79f4c5adf82910bfac6f171414e7d9e6871
Helena <helenagkraus@gmail.com>
2024-04-18 21:22:29 +0200

    sd

--- _personale/moltiplica_due_numeri.py
+++ _personale/moltiplica_due_numeri.py
@@ -5,8 +5,12 @@ import sys
 if len(sys.argv) != 3:
     print("Il numero di parametri passati non è corretto. Devi inserire 2 numeri.")
 else:
-    primo_numero = float(sys.argv[1])
-    secondo_numero = float(sys.argv[2])
+    try:
+        primo_numero = float(sys.argv[1])
+        
+    except ValueError
 
+
+secondo_numero = float(sys.argv[2])
     risultato = primo_numero * secondo_numero
     print(f"il risultato di {primo_numero} per {secondo_numero} è uguale a {risultato}")
