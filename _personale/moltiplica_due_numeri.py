132b8332b147951b44e05a77aff1fa21a549af45
Helena <helenagkraus@gmail.com>
2024-04-18 21:12:25 +0200

    dsff

--- _personale/moltiplica_due_numeri.py
+++ _personale/moltiplica_due_numeri.py
@@ -1,3 +1,12 @@
 import sys
 
-print(sys.argv)
+
+# path_script = sys.argv[0]
+if len(sys.argv) != 3:
+    print("Il numero di parametri passati non è corretto. Devi inserire 2 numeri.")
+else:
+    primo_numero = float(sys.argv[1])
+    secondo_numero = float(sys.argv[2])
+
+    risultato = primo_numero * secondo_numero
+    print(f"il risultato di {primo_numero} per {secondo_numero} è uguale a {risultato}")
