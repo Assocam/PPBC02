2d6a167c3fa9d5c460cc282a705bc7177cc5df6c
bortolomeovarnava <costelbreazu74@gmail.com>
2024-06-06 20:47:16 +0200

    ufewipue

--- backend.py
+++ backend.py
@@ -1,7 +1,16 @@
-from flask import Flask
-import requests
+from flask import Flask, request
+
 
 backend = Flask('applicazioneBE')
+
+@backend.route('/ciao', methods=['POST','GET'])
+def rottaCiao():
+
+    come = request.method
+
+    return f'hai chiamato la rotta ciao  con metodo {come}'
+
+
 
 if (__name__ == '__main__'):
     backend.run(host = '0.0.0.0',port = 80)
