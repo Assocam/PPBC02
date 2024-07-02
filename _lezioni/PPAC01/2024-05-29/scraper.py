b406da18ad40062aeaa74bccb1dfb03a2f1ee5ad
PPBC01-21 <industrie.creative@gmail.com>
2024-07-02 21:27:19 +0200

    nuova lezione

--- _lezioni/PPAC01/2024-05-29/scraper.py
+++ _lezioni/PPAC01/2024-05-29/scraper.py
@@ -17,31 +17,38 @@ if result.status_code == 200:
     soup = BeautifulSoup(result.content, 'html.parser')
 
     # Seleziona gli elementi HTML corrispondenti ai prodotti desiderati
-    products = soup.select('div.items__item.item-card')
+    # products = soup.select('div.items__item.item-card')
+    products = soup.select('div[class*=SmallCard-module_card__]')
     # pprint(products)  # Stampa la lista di prodotti (opzionale)
 
     # Lista per memorizzare gli oggetti di interesse
     items = []
 
-    # Itera sui prodotti trovati
+
+    # # Itera sui prodotti trovati
     for adv in products:
-        # Estrae i dati di interesse dal prodotto corrente
+    #     # Estrae i dati di interesse dal prodotto corrente
         data = {
             'name': adv.select('h2[class*=ItemTitle-module_item-title__]'),  # Nome del prodotto
-            'price': adv.select("p[class*=index-module_price__]"),  # Prezzo del prodotto
+            'price': adv.select("p[class*=SmallCard-module_price__]"),  # Prezzo del prodotto
             'town': adv.select('span[class*=index-module_town__]'),  # Città del venditore
-        }
+            'province': adv.select('span.city'),
+        }   
 
-        # Elabora i dati estratti
+        # for key in data:
+        #     value = data[key]
+
         for key, value in data.items():
             if value:
-                # Se l'elemento è presente, estrai il testo
                 data[key] = value[0].get_text()
             else:
-                # Se l'elemento non è presente, imposta il valore a None
                 data[key] = None
-        
+            
+        # print(type(data['price']))
+        if data['price'] is not None:
+            data['price'] = data['price'].replace('\xa0',' ').replace('Spedizione disponibile', '')
+
         items.append(data)
 
-    # Stampa i dati estratti
+    # # Stampa i dati estratti
     pprint(items)
